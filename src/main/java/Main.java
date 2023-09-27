b8e6f55b122f0c98c165e51dc46c5afd91234cce
MrJustMeDahl <nico.dahl93@gmail.com>
2023-09-27 11:08:24 +0200

    Commented out main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,24 +8,24 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Stock> stocks = new ArrayList<>();
-
-        stocks.addAll(Webscraping.getInstance().findBestStocks());
-        stocks.addAll(Webscraping.getInstance().findWorstStocks());
-
-        for (Stock s : stocks) {
-            try {
-                Thread.sleep(5500);
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-            }
-            try {
-                StockAPIEnricher.getInstance().getIdForStock(s);
-                StockAPIEnricher.getInstance().getRiskForStock(s);
-            } catch (ApiException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+//        List<Stock> stocks = new ArrayList<>();
+//
+//        stocks.addAll(Webscraping.getInstance().findBestStocks());
+//        stocks.addAll(Webscraping.getInstance().findWorstStocks());
+//
+//        for (Stock s : stocks) {
+//            try {
+//                Thread.sleep(5500);
+//            } catch (InterruptedException e) {
+//                System.out.println(e.getMessage());
+//            }
+//            try {
+//                StockAPIEnricher.getInstance().getIdForStock(s);
+//                StockAPIEnricher.getInstance().getRiskForStock(s);
+//            } catch (ApiException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
 
 //        for (Stock s : stocks) {
 //            if (IndustryDAO.getInstance().doesIndustryExist(s.getIndustry())) {
